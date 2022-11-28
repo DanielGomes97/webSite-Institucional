5770b8192ce69959512e167f1e1d2f3dd8972175
DanielGomes97 <danielgommes1997@hotmail.com>
2022-11-28 18:44:55 -0300

    Primeiro projeto no github

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>DMAPI - Criação de Websites, design e serviços de infraestrutura de redes!</title>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<div class="header-1">
+				<!-- SESSAO DO LOGO -->
+					<div class="logo">	
+						<a href="#">
+							<img src="assets/images/logo/logo.png" alt="" class="logo">
+						</a>
+					</div>
+
+					<!-- SESSAO DO MENU -->
+					<div class="menu-principal">
+						<nav>
+							<ul>
+								<li><a href="#">Inicio</a></li>
+								<li><a href="#sobre">Sobre</a></li>
+								<li><a href="#qm-somos">Quem somos</a></li>
+								<li><a href="#contato">Contato</a></li>
+							</ul>
+						</nav>
+					</div>
+
+					<!-- SESSAO DO LADO DIREITO > -->
+					<div class="rede-social">
+						<ul>
+							<li><a href="https://www.facebook.com/dmapi2021" target="_blank"><img src="assets/images/icones/face2.png" title="Facebook"></a></li>
+							<li><a href="#" target="_blank"><img src="assets/images/icones/insta2.png" title="Instagram"></a></li>
+							<li><a href="#" target="_blank"><img src="assets/images/icones/tw2.png" title="Twitter"></a></li>
+							<li><a href="#" target="_blank"><img src="assets/images/icones/discord2.png" title="Discord"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+
+
+
+		<main>
+			<div class="container--banner">
+				<!-- SESSAO BANNER -->
+				<div class="banner-principal">	
+					<a href="#">
+						<img src="assets/images/fundo/banner.png" alt="">
+					</a>
+				</div>
+				<a href="#sobre" class="seta-img">
+					<img src="assets/images/slider/seta-baixo.png" class="seta-baixo">
+				</a>
+			</div>
+
+
+
+			<div class="main-bg-light" id="sobre">
+				<div class="container">		
+					<div class="sessao">
+						<h1>SOBRE A EMPRESA</h1>
+					</div>
+					<div class="sobre">
+						<div class="sobre-img">
+							<img src="assets/images/fundo/2.jpg">
+						</div>
+						<div class="sobre-conteudo">
+							<h1 class="titulo">OBJETIVOS</h1>
+							<h5 class="subtitulo">Seu sucesso começa aqui</h5>					
+							<p class="paragrafo">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+								 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+								 make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+								 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+								 Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
+								 of Lorem Ipsum.
+								<br><br>
+								There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, 
+								by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage 
+								of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum 
+								generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the 
+								Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+								to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, 
+								injected humour, or non-characteristic words etc.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="main-bg-dark" id="qm-somos">
+				<div class="container">	
+					<div class="sessao-light">
+						<h1>QUEM SOMOS</h1>
+					</div>
+					<div class="qm-somos">
+						<div class="qm-somos-conteudo">
+							<h1 class="titulo">NOSSA EQUIPE</h1>
+							<h5 class="subtitulo">Seu sucesso começa aqui</h5>					
+							<p class="paragrafo">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+								 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+								 make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+								 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+								 Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
+								 of Lorem Ipsum.
+								<br><br>
+								There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, 
+								by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage 
+								of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum 
+								generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the 
+								Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+								to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, 
+								injected humour, or non-characteristic words etc.
+
+								<br><br>
+								There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, 
+								by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage 
+								of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum 
+								generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the 
+								Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+								to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, 
+								injected humour, or non-characteristic words etc.
+							</p>
+						</div>
+						<div class="qm-somos-img">
+							<img src="assets/images/img/6.jpg">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="main-bg-light" id="contato">
+				<div class="container">
+					<div class="sessao">
+						<h1>CONTATO</h1>
+					</div>
+					<div class="qm-somos">
+						<div class="contato-img">
+							<img src="assets/images/img/8.jpg">
+						</div>
+						<div class="contato-conteudo">
+							<h1 class="titulo">Preencha o formulario abaixo</h1>
+							<h5 class="subtitulo">Entraremos em contato o mais rapido possivel</h5>					
+			
+							<form action="" method="POST" class="contato-frm">
+						      <fieldset>
+						        
+						        <input id="nome" name="nome" type="text" placeholder="Digite seu nome" value="" requered />
+
+						        <input id="email" name="email" type="text" placeholder="Digite seu e-mail" value="" />
+
+
+						        <input id="telefone" name="telefone" type="text" placeholder="Digite seu telefone" value="" />
+
+
+					            <select id="assunto" name="assunto">
+					                <option value="Orçamento"  >Orçamento</option>
+					                <option value="Visita técnica"  >Visita Técnina</option>
+					                <option value="Dúvidas"  >Dúvidas</option>
+					                <option value="Outras"  >Outros</option>
+					            </select>
+
+						        <textarea id="mensagem" name="mensagem" rows="7" placeholder="Deixe uma mensagem para vermos!"></textarea>
+						        </label><br><br>
+						 
+						        <input name="submit" type="submit" value="Enviar e-mail" style="width: auto;" />
+						      </fieldset>
+						    </form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+
+		<footer>
+			<div class="container">
+				<div class="footer-info">
+					Site desenvolvido por <a href="#">DMAPI</a>.<br>
+					Entre em nosso sites e venha conhecer nossos serviços!
+				
+					<div class="footer-social">
+						<ul>
+							<li><a href="#" target="_blank"><img src="assets/images/icones/face2.png" title="Facebook"></a></li>
+							<li><a href="#" target="_blank"><img src="assets/images/icones/insta2.png" title="Instagram"></a></li>
+							<li><a href="#" target="_blank"><img src="assets/images/icones/tw2.png" title="Twitter"></a></li>
+							<li><a href="#" target="_blank"><img src="assets/images/icones/discord2.png" title="Discord"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>	
+		</footer>
+		<section class="footer-copyright">
+			<div class="container">
+				<div class="copyright">
+					<strong>DMAPI</strong> 2021 - TODOS DIREITOS RESERVADOS
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
